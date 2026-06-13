aae6c741d12722bd915d318e401c21651dd325d3
Shabab Haider Siddique <shabab.h.siddique@gmail.com>
2026-06-13 22:56:08 +0600

    added no crawl

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8"/>
     <title>bilgi, Education Toys</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @stack('head')
