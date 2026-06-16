4eb6e6142a63e1d0be66bb59b6c3c7f51ce4de07
Shabab Haider Siddique <shabab.h.siddique@gmail.com>
2026-06-16 14:34:05 +0600

    admin model update

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable; // Or just Authenticatable
 use Illuminate\Notifications\Notifiable;
 
-class Admin extends Authenticatable
+class Admin extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     protected $fillable = [
         'name',
